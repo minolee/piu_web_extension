70211063b69f4b324e3518107d6b7ec8fcfa3009
Minho <lmh30002@gmail.com>
2023-12-02 14:15:31 +0900

    1.2.1

--- contents/constant.js
+++ contents/constant.js
@@ -6,6 +6,8 @@ const is_best_score_page = document.URL.split("/").slice(-1)[0].split("?")[0] ==
 const is_playdata_page = document.URL.split("/").slice(-1)[0].split("?")[0] == "play_data.php"
 const lazy_values = {}
 
+const IMG_API_URL = "https://piu.minolee.xyz/image/chart"
+
 const PLATE_LIST = ["pg", "ug", "eg", "sg", "mg", "tg", "fg", "rg"]
 const RANK_LIST = ["sss_p", "sss", "ss_p", "ss", "s_p", "s", "aaa_p", "aaa", "aa_p", "aa", "a_p", "a", "b"]
 const G_IMAGES = (game) => {
@@ -23,4 +25,19 @@ const RANK_IMAGES = (rank, clear) => {
 
 const img_wrapper = (img) => {
     return `<div class="img"><img src="${img}" alt></div>`
+}
+
+const createElement = (tag, className, innerHTML = "") => {
+    const element = document.createElement(tag)
+    for (const _className of className.split(" ")) {
+        element.classList.add(_className)
+    }
+    element.innerHTML = innerHTML
+    return element
+}
+const createHierarchy = (...elements) => {
+    for (let i = 0; i < elements.length - 1; i++) {
+        elements[i].appendChild(elements[i + 1])
+    }
+    return elements[0]
 }
--- contents/play_data_dom.js
+++ contents/play_data_dom.js
@@ -3,25 +3,101 @@ function generate_mod_rating(user_data) {
     // TODO
 }
 
+async function get_checklist_img(sd, lv) {
+    const user_data = await getObjectFromLocalStorage(lazy_values["user"])
+    console.log(user_data)
+    console.log(sd, lv)
+    console.log(lazy_values)
+    if (lv == -1) {
+        alert("레벨을 선택해주세요!")
+        return
+    }
+    if (lv < 19 || lv > 24) {
+        alert("준비중입니다.")
+        return
+    }
+    let x = lazy_values["user"].split(" ")
+    const user_name = x[0]
+    const user_tag = x[1]
+    const request_form = {
+        "user_name": user_name + user_tag,
+        "type": sd,
+        "level": lv,
+        "best_scores": user_data.best_scores
+    }
+    // console.log(request_form)
+    // console.log(request_form)
+    const response = await fetch(IMG_API_URL, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(request_form)
+    }).then(response => response.blob())
+    // console.log(response)
+    const handle = await showSaveFilePicker({
+        suggestedName: `${user_name}${user_tag}_${sd}_${lv}.png`,
+        types: [{
+            description: 'PNG Image',
+            accept: {
+                'image/png': ['.png'],
+            },
+        }]
+    })
+    const writable = await handle.createWritable();
+    await writable.write(response);
+    await writable.close();
+}
+
+
 async function modify_playdata_dom() {
     let all = document.URL.split("?").length == 1
 
     const lv = all ? -1 : Number(document.URL.split("=").slice(-1)[0])
     all = all || lv == 0
     const target = document.getElementsByClassName("plate_w")[0].getElementsByClassName("list flex vc wrap")[0]
-    console.log(target)
+    // console.log(target)
     const user_data = await getObjectFromLocalStorage(lazy_values["user"])
-    console.log(user_data)
+    // console.log(user_data)
     const rank_counts = {}
+
+
+    const gen_menu_container = createElement("ul", "dp2 flex")
+    const checklist_gen_menu = [
+        [createElement("li", "single_checklist_gen"), createElement("span", "tt", "싱글 체크리스트 작성")],
+        [createElement("li", "double_checklist_gen"), createElement("span", "tt", "더블 체크리스트 작성")]
+    ]
+    checklist_gen_menu[0][0].addEventListener("click", async () => {
+        await get_checklist_img("single", lv)
+    })
+    checklist_gen_menu[1][0].addEventListener("click", async () => {
+        await get_checklist_img("double", lv)
+    })
+    gen_menu_container.appendChild(createHierarchy(...checklist_gen_menu[0]))
+    gen_menu_container.appendChild(createHierarchy(...checklist_gen_menu[1]))
+
+    const checklist_gen_container = createHierarchy(
+        createElement("div", "subMenu_wrap"),
+        createElement("div", "box1"),
+        createElement("ul", "gnb flex"),
+        createElement("li", "m_my_page on"),
+        gen_menu_container
+    )
+
+    const parent = document.getElementsByClassName("pageWrap box1")[0]
+    parent.insertBefore(checklist_gen_container, parent.childNodes[2])
+
+
+
     RANK_LIST.forEach(rank => {
         rank_counts[rank] = 0
     })
     user_data.best_scores.forEach(score => {
         if (all || score.level == lv) {
-            console.log(score)
-            console.log(get_rank(score.score))
-            console.log(Object.keys(rank_counts).length - get_rank(score.score))
-            console.log(RANK_LIST[Object.keys(rank_counts).length - get_rank(score.score) - 1])
+            // console.log(score)
+            // console.log(get_rank(score.score))
+            // console.log(Object.keys(rank_counts).length - get_rank(score.score))
+            // console.log(RANK_LIST[Object.keys(rank_counts).length - get_rank(score.score) - 1])
             rank_counts[RANK_LIST[Object.keys(rank_counts).length - get_rank(score.score) - 1]]++
         }
     })
